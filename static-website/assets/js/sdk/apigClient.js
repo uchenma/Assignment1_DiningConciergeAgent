1984ea66a923751352ad06443d7b6e48ffda584f
Nigel Schuster <nigel.schusters@googlemail.com>
2020-10-23 08:39:11 -0400

    Cognito integration

--- static-website/assets/js/sdk/apigClient.js
+++ static-website/assets/js/sdk/apigClient.js
@@ -14,110 +14,134 @@
  */
 
 var apigClientFactory = {};
-apigClientFactory.newClient = function (config) {
-    var apigClient = { };
-    if(config === undefined) {
-        config = {
-            accessKey: '',
-            secretKey: '',
-            sessionToken: '',
-            region: '',
-            apiKey: undefined,
-            defaultContentType: 'application/json',
-            defaultAcceptType: 'application/json'
-        };
-    }
-    if(config.accessKey === undefined) {
-        config.accessKey = '';
-    }
-    if(config.secretKey === undefined) {
-        config.secretKey = '';
-    }
-    if(config.apiKey === undefined) {
-        config.apiKey = '';
-    }
-    if(config.sessionToken === undefined) {
-        config.sessionToken = '';
-    }
-    if(config.region === undefined) {
-        config.region = 'us-east-2';
-    }
-    //If defaultContentType is not defined then default to application/json
-    if(config.defaultContentType === undefined) {
-        config.defaultContentType = 'application/json';
-    }
-    //If defaultAcceptType is not defined then default to application/json
-    if(config.defaultAcceptType === undefined) {
-        config.defaultAcceptType = 'application/json';
+apigClientFactory.newClient = function(config) {
+  var apigClient = {};
+  if (config === undefined) {
+    config = {
+      accessKey: "",
+      secretKey: "",
+      sessionToken: "",
+      region: "",
+      apiKey: undefined,
+      defaultContentType: "application/json",
+      defaultAcceptType: "application/json"
+    };
+  }
+  if (config.accessKey === undefined) {
+    config.accessKey = "";
+  }
+  if (config.secretKey === undefined) {
+    config.secretKey = "";
+  }
+  if (config.apiKey === undefined) {
+    config.apiKey = "";
+  }
+  if (config.sessionToken === undefined) {
+    config.sessionToken = "";
+  }
+  if (config.region === undefined) {
+    config.region = "us-east-1";
+  }
+  //If defaultContentType is not defined then default to application/json
+  if (config.defaultContentType === undefined) {
+    config.defaultContentType = "application/json";
+  }
+  //If defaultAcceptType is not defined then default to application/json
+  if (config.defaultAcceptType === undefined) {
+    config.defaultAcceptType = "application/json";
+  }
+
+  // extract endpoint and path from url
+  var invokeUrl = "https://um9upeh23j.execute-api.us-east-1.amazonaws.com/prod";
+  var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
+  var pathComponent = invokeUrl.substring(endpoint.length);
+
+  var sigV4ClientConfig = {
+    accessKey: config.accessKey,
+    secretKey: config.secretKey,
+    sessionToken: config.sessionToken,
+    serviceName: "execute-api",
+    region: config.region,
+    endpoint: endpoint,
+    defaultContentType: config.defaultContentType,
+    defaultAcceptType: config.defaultAcceptType
+  };
+
+  var authType = "NONE";
+  if (
+    sigV4ClientConfig.accessKey !== undefined &&
+    sigV4ClientConfig.accessKey !== "" &&
+    sigV4ClientConfig.secretKey !== undefined &&
+    sigV4ClientConfig.secretKey !== ""
+  ) {
+    authType = "AWS_IAM";
+  }
+  console.log(sigV4ClientConfig);
+
+  var simpleHttpClientConfig = {
+    endpoint: endpoint,
+    defaultContentType: config.defaultContentType,
+    defaultAcceptType: config.defaultAcceptType
+  };
+
+  var apiGatewayClient = apiGateway.core.apiGatewayClientFactory.newClient(
+    simpleHttpClientConfig,
+    sigV4ClientConfig
+  );
+
+  apigClient.chatbotPost = function(params, body, additionalParams) {
+    if (additionalParams === undefined) {
+      additionalParams = {};
     }
 
+    apiGateway.core.utils.assertParametersDefined(params, ["body"], ["body"]);
 
-    // extract endpoint and path from url
-    var invokeUrl = 'https://um9upeh23j.execute-api.us-east-1.amazonaws.com/prod';
-    var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
-    var pathComponent = invokeUrl.substring(endpoint.length);
-
-    var sigV4ClientConfig = {
-        accessKey: config.accessKey,
-        secretKey: config.secretKey,
-        sessionToken: config.sessionToken,
-        serviceName: 'execute-api',
-        region: config.region,
-        endpoint: endpoint,
-        defaultContentType: config.defaultContentType,
-        defaultAcceptType: config.defaultAcceptType
+    var chatbotPostRequest = {
+      verb: "post".toUpperCase(),
+      path:
+        pathComponent +
+        uritemplate("/chatbot").expand(
+          apiGateway.core.utils.parseParametersToObject(params, [])
+        ),
+      headers: apiGateway.core.utils.parseParametersToObject(params, []),
+      queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+      body: body
     };
 
-    var authType = 'NONE';
-    if (sigV4ClientConfig.accessKey !== undefined && sigV4ClientConfig.accessKey !== '' && sigV4ClientConfig.secretKey !== undefined && sigV4ClientConfig.secretKey !== '') {
-        authType = 'AWS_IAM';
+    return apiGatewayClient.makeRequest(
+      chatbotPostRequest,
+      authType,
+      additionalParams,
+      config.apiKey
+    );
+  };
+
+  apigClient.chatbotOptions = function(params, body, additionalParams) {
+    if (additionalParams === undefined) {
+      additionalParams = {};
     }
 
-    var simpleHttpClientConfig = {
-        endpoint: endpoint,
-        defaultContentType: config.defaultContentType,
-        defaultAcceptType: config.defaultAcceptType
+    apiGateway.core.utils.assertParametersDefined(params, [], ["body"]);
+
+    var chatbotOptionsRequest = {
+      verb: "options".toUpperCase(),
+      path:
+        pathComponent +
+        uritemplate("/chatbot").expand(
+          apiGateway.core.utils.parseParametersToObject(params, [])
+        ),
+      headers: apiGateway.core.utils.parseParametersToObject(params, []),
+      queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+      body: body
     };
 
-    var apiGatewayClient = apiGateway.core.apiGatewayClientFactory.newClient(simpleHttpClientConfig, sigV4ClientConfig);
+    return apiGatewayClient.makeRequest(
+      chatbotOptionsRequest,
+      authType,
+      additionalParams,
+      config.apiKey
+    );
+  };
 
-
-
-    apigClient.chatbotPost = function (params, body, additionalParams) {
-        if(additionalParams === undefined) { additionalParams = {}; }
-
-        apiGateway.core.utils.assertParametersDefined(params, ['body'], ['body']);
-
-        var nluPostRequest = {
-            verb: 'post'.toUpperCase(),
-            path: pathComponent + uritemplate('/chatbot').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
-            headers: apiGateway.core.utils.parseParametersToObject(params, []),
-            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
-            body: body
-        };
-
-
-        return apiGatewayClient.makeRequest(nluPostRequest, authType, additionalParams, config.apiKey);
-    };
-
-
-    apigClient.chatbotOptions = function (params, body, additionalParams) {
-        if(additionalParams === undefined) { additionalParams = {}; }
-
-        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
-
-        var nluOptionsRequest = {
-            verb: 'options'.toUpperCase(),
-            path: pathComponent + uritemplate('/chatbot').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
-            headers: apiGateway.core.utils.parseParametersToObject(params, []),
-            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
-            body: body
-        };
-
-
-        return apiGatewayClient.makeRequest(nluOptionsRequest, authType, additionalParams, config.apiKey);
-    };
-
-
-    return apigClient;
+  return apigClient;
 };
